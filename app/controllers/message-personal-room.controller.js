a11a2f7870d78a7235026029be1b961824f0d53c
Alexander Melara Avalos <lexmelara@gmail.com>
2018-10-18 17:59:48 -0600

    Integration function getOrCreateMessageRoom

--- app/controllers/message-personal-room.controller.js
+++ app/controllers/message-personal-room.controller.js
@@ -17,6 +17,48 @@ exports.getMessages = function (req, res) {
             res.json(messagePersonalRoom);
         });
 }
+exports.getOrCreateMesaggeRoom = function (req, res) {
+
+    //Check if exist the room
+    MessagePersonalRoom.find(
+        {
+            $or: [
+                { user_one: req.query.id_one, user_two: req.query.id_two },
+                { user_two: req.query.id_one, user_one: req.query.id_two }
+            ]
+        }
+    )
+        .exec(function (err, messagePersonalRoom) {
+            if (err) {
+                return res.json({ status: 500, message: "Error in transaction" });
+            }
+            else if (!messagePersonalRoom) {
+                return res.json({ status: 404, message: "Users not found" });
+            }
+            //If exist
+            if (messagePersonalRoom.length) {
+                return res.json({ status: 200, room: messagePersonalRoom[0]._id, message: "Success" });
+            }
+
+            //if not exist create
+            else {
+                var messagePersonalR = new MessagePersonalRoom({
+                    user_one: req.body.user_one,
+                    user_two: req.body.user_two,
+                    status: true,
+                })
+                messagePersonalR.save(function (err) {
+                    if (err) {
+                        return res.json({ status: 500, message: "Error in transaction" });
+                    }
+                    else {
+                        return res.json({ status: 200, room: messagePersonalR[0]._id, message: "Success" });
+                    }
+                })
+            }
+        });
+
+}
 
 exports.createMessage = function (req, res) {
     var messagePersonalRoom = new MessagePersonalRoom(req.body);
